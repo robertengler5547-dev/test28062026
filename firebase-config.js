26f0aad048f8f1d0f8aa2e10ab4ba69e082c7153
robertengler5547-dev <robert.engler5547@gmail.com>
2026-06-29 02:26:39 +0200

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,7 @@
+// Automatisch generiert von setup-firebase.ps1
+window.FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyCG01QMVxipv-WeacxDiaszYCaYIkMUcBs',
+  authDomain: 'mission-pfotenklick-rheine.firebaseapp.com',
+  databaseURL: 'https://mission-pfotenklick-rheine-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'mission-pfotenklick-rheine',
+};
